001e2073d1b3d6b6b4f649d97ae99093a1742b08
Isty001 <isty001@gmail.com>
2015-11-04 23:20:53 +0100

    Test

--- tests/Pipeline/FilterTest.php
+++ tests/Pipeline/FilterTest.php
@@ -3,8 +3,9 @@
 namespace Tests\Pipeline;
 
 use Pipeline\Pipeline;
+use Tests\Resources\Author;
 
-class FilterTest extends AbstractPipelineTest
+class FilterTest extends \PHPUnit_Framework_TestCase
 {
     public function testFilter()
     {
@@ -30,5 +31,34 @@ class FilterTest extends AbstractPipelineTest
     {
         list($author1, $author2, $author3, $author4, $author5) = $this->createAuthors();
         return [0 => $author2];
+    }
+
+    /**
+     * @return array
+     */
+    private function createAuthors() : array
+    {
+        $author1 = new Author();
+        $author1->setName('Hello');
+        $author1->setAge(26);
+
+        $author2 = new Author();
+        $author2->setName('Hi');
+        $author2->setAge(34);
+
+        $author3 = new Author();
+        $author3->setName('Hi');
+        $author3->setAge(19);
+
+
+        $author4 = new Author();
+        $author4->setName('Asd10');
+        $author4->setAge(60);
+
+        $author5 = new Author();
+        $author5->setName('Hi');
+        $author5->setAge(20);
+
+        return [$author1, $author2, $author3, $author4, $author5];
     }
 }
